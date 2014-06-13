0bd23d7d9bb5f9d4f10e715bf6ecd83bf74c9e96
Matt Tanner <matt@firestarterdigital.com>
2014-06-12 21:39:10 -0400

    Initial commit
    Initial commit

---
+++ js/global.js
@@ -0,0 +1,20 @@
+// Modernizr Queries
+function mediaQueries() {
+	// Mobile and Tablet Only
+	if (Modernizr.mq('only screen and (max-width: 64em)')) {
+    
+  } 
+	// Desktop and Up
+	else {
+		
+	}
+}
+
+// document.ready
+jQuery(document).ready(function() {
+	mediaQueries();
+});
+
+// window.resize
+jQuery(window).resize(function() {
+});
